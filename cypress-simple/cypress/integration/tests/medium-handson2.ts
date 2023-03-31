91d5bc229b7d8ea8026936c479d08ef24aa6296b
Tejesh-VarmaW1 <tejesh2001work1@gmail.com>
2023-03-31 19:42:14 +0530

    new

--- cypress-simple/cypress/integration/tests/medium-handson2.ts
+++ cypress-simple/cypress/integration/tests/medium-handson2.ts
@@ -1,8 +1,15 @@
 /// <reference types="Cypress" />
+import "cypress-real-events/support";
 
 describe("Medium handson 2", function () {
   it("Booking flight", function () {
-    cy.visit("https://www.booking.com/");
-    cy.get(".b8ef7618ca > .bb0b3e18ca > .fc63351294").click();
+    cy.visit("https://www.manyavar.com/");
+    cy.get("a[href='/category/men']")
+      .click()
+      .get(".row > :nth-child(1) > :nth-child(2) > :nth-child(2) > a")
+      .click();
+    cy.get(".accordianTitle0sFilters").click();
+    cy.get("input[type='checkbox']").contains("Indo Western").check();
+    cy.get("input[type='checkbox']").contains("Sherwani").check();
   });
 });
